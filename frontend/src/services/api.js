349c451d75457881ffe0eb94a307d81f60b279b9
Abdulrahman <abdulrahamnm55@icloud.com>
2026-06-14 14:31:57 +0300

    fix: add REACT_APP_API_URLfor vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import { data } from "autoprefixer";
 import axios from "axios";
 
 const API  = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000'
 })
 
 API.interceptors.request.use(config => {
